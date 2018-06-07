3fc81dc78c1da282d0c82935bf3435fbd9f64c33
keitakn <keita.koga.work@gmail.com>
2018-06-07 11:01:50 +0900

    modify #20 separated the test of the "info" method into another class

---
+++ tests/Logger/InfoTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Nekonomokochan\Tests\Logger;
+
+use Nekonomokochan\PhpJsonLogger\LoggerBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class InfoTest
+ *
+ * @package Nekonomokochan\Tests\Logger
+ * @see \Nekonomokochan\PhpJsonLogger\Logger::info
+ */
+class InfoTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        // Delete the log file to assert the log file
+        $defaultFile = '/tmp/php-json-logger-' . date('Y-m-d') . '.log';
+        if (file_exists($defaultFile)) {
+            unlink($defaultFile);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function outputInfoLog()
+    {
+        $context = [
+            'title' => 'Test',
+            'price' => 4000,
+            'list'  => [1, 2, 3],
+            'user'  => [
+                'id'   => 100,
+                'name' => 'keitakn',
+            ],
+        ];
+
+        $loggerBuilder = new LoggerBuilder();
+        $logger = $loggerBuilder->build();
+        $logger->info('🐱', $context);
+
+        $resultJson = file_get_contents('/tmp/php-json-logger-' . date('Y-m-d') . '.log');
+        $resultArray = json_decode($resultJson, true);
+
+        echo "\n ---- Output Log Begin ---- \n";
+        echo json_encode($resultArray, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        echo "\n ---- Output Log End   ---- \n";
+
+        $expectedLog = [
+            'log_level'         => 'INFO',
+            'message'           => '🐱',
+            'trace_id'          => $logger->getTraceId(),
+            'file'              => __FILE__,
+            'line'              => 45,
+            'context'           => $context,
+            'remote_ip_address' => '127.0.0.1',
+            'user_agent'        => 'unknown',
+            'datetime'          => $resultArray['datetime'],
+            'timezone'          => date_default_timezone_get(),
+            'process_time'      => $resultArray['process_time'],
+        ];
+
+        $this->assertSame('PhpJsonLogger', $logger->getMonologInstance()->getName());
+        $this->assertSame($expectedLog, $resultArray);
+    }
+}
