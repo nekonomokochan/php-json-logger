39c1645243344e62f62b20dd3f0d94c0d344fa93
keitakn <keita.koga.work@gmail.com>
2018-06-07 11:55:46 +0900

    add #20 separated the test of the "critical" method into another class

---
+++ tests/Logger/CriticalTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace Nekonomokochan\Tests\Logger;
+
+use Nekonomokochan\PhpJsonLogger\LoggerBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CriticalTest
+ *
+ * @package Nekonomokochan\Tests\Logger
+ * @see \Nekonomokochan\PhpJsonLogger\Logger::critical
+ */
+class CriticalTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        // Delete the log file to assert the log file
+        $defaultFile = '/tmp/php-json-logger-' . date('Y-m-d') . '.log';
+        if (file_exists($defaultFile)) {
+            unlink($defaultFile);
+        }
+    }
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function outputCriticalLog()
+    {
+        $exception = new \ErrorException('TestCritical', 500);
+        $context = [
+            'name'  => 'keitakn',
+            'email' => 'dummy@email.com',
+        ];
+
+        $loggerBuilder = new LoggerBuilder();
+        $logger = $loggerBuilder->build();
+        $logger->critical($exception, $context);
+
+        $resultJson = file_get_contents('/tmp/php-json-logger-' . date('Y-m-d') . '.log');
+        $resultArray = json_decode($resultJson, true);
+
+        echo "\n ---- Output Log Begin ---- \n";
+        echo json_encode($resultArray, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        echo "\n ---- Output Log End   ---- \n";
+
+        $expectedLog = [
+            'log_level'         => 'CRITICAL',
+            'message'           => 'ErrorException',
+            'trace_id'          => $logger->getTraceId(),
+            'file'              => __FILE__,
+            'line'              => 39,
+            'context'           => $context,
+            'remote_ip_address' => '127.0.0.1',
+            'user_agent'        => 'unknown',
+            'datetime'          => $resultArray['datetime'],
+            'timezone'          => date_default_timezone_get(),
+            'process_time'      => $resultArray['process_time'],
+            'errors'            => [
+                'message' => 'TestCritical',
+                'code'    => 500,
+                'file'    => __FILE__,
+                'line'    => 31,
+                'trace'   => $resultArray['errors']['trace'],
+            ],
+        ];
+
+        $this->assertSame('PhpJsonLogger', $logger->getMonologInstance()->getName());
+        $this->assertSame($expectedLog, $resultArray);
+    }
+}
