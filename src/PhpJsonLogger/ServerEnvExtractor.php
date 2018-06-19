02790852ed29b7cd067ff82b500a2dd3476c8045
keitakn <keita.koga.work@gmail.com>
2018-06-19 12:20:03 +0900

    :recycle: #52 Create trait ServerEnvExtractor and move server variable acquisition processing

---
+++ src/PhpJsonLogger/ServerEnvExtractor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kogakeita
+ * Date: 2018/06/19
+ * Time: 11:59
+ */
+
+namespace Nekonomokochan\PhpJsonLogger;
+
+
+class ServerEnvExtractor
+{
+
+}
