6449b0fc7ea402cea1a07a43605c0161298fb789
keitakn <keita.koga.work@gmail.com>
2018-06-19 12:20:42 +0900

    :recycle: #52 Create "trait ServerEnvExtractor" and move server variable acquisition processing

--- src/PhpJsonLogger/ServerEnvExtractor.php
+++ src/PhpJsonLogger/ServerEnvExtractor.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: kogakeita
- * Date: 2018/06/19
- * Time: 11:59
- */
 
 namespace Nekonomokochan\PhpJsonLogger;
 
-
-class ServerEnvExtractor
+/**
+ * Trait ServerEnvExtractor
+ *
+ * @package Nekonomokochan\PhpJsonLogger
+ */
+trait ServerEnvExtractor
 {
+    /**
+     * @return string
+     */
+    public function extractRemoteIpAddress()
+    {
+        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
+            $ipList = explode(',', $ip);
+
+            return $ipList[0];
+        }
+
+        if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+
+        return '127.0.0.1';
+    }
+
+    /**
+     * @return string
+     */
+    public function extractUserAgent()
+    {
+        if (array_key_exists('HTTP_USER_AGENT', $_SERVER)) {
+            return $_SERVER['HTTP_USER_AGENT'];
+        }
+
+        return 'unknown';
+    }
 }
